37df25af3225ec3a026eb36da6edaed96fb46f0c
manu <manuel.guevara@openpay.mx>
2018-07-27 10:24:08 -0500

    fix minor errors, add testing level customer

--- src/test/integration/mx/bancomer/core/client/full/CustomerTokenChargesTest.java
+++ src/test/integration/mx/bancomer/core/client/full/CustomerTokenChargesTest.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import mx.bancomer.client.Charge;
 import mx.bancomer.client.Customer;
 import mx.bancomer.client.Token;
+import mx.bancomer.client.core.BancomerAPI;
 import mx.bancomer.client.core.requests.parameters.Parameter;
+import mx.bancomer.client.core.requests.parameters.ParameterContainer;
 import mx.bancomer.client.core.requests.parameters.SingleParameter;
 import mx.bancomer.client.exceptions.ServiceException;
 import mx.bancomer.client.exceptions.ServiceUnavailableException;
@@ -18,20 +20,42 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 @Slf4j
 public class CustomerTokenChargesTest extends BaseTest {
-
+    
     private Customer customer;
+    private ParameterContainer customerRequest;
 
     @Before
     public void setUp() throws Exception {
-        this.customer = this.api.customers().create(
-                new Customer().name("John").lastName("Doe").email("john.doe@mail.com")
-                .phoneNumber("55-25634013"));
+        String merchantId = "mdopyxbg6cacgbdvqqxd";
+        String apiKey = "sk_36ad147b36234a30bc279031ac17e1a6";
+        String endpoint = "https://sandbox-api.openpay.mx/";
+        this.api = new BancomerAPI(endpoint, apiKey, merchantId);
+        TimeZone.setDefault(TimeZone.getTimeZone("Mexico/General"));
+
+        ParameterContainer address = new ParameterContainer("address");
+        address.addValue("line1", "Calle Morelos #12 - 11");
+        address.addValue("line2", "Colonia Centro");           // Optional
+        address.addValue("line3", "Cuauhtémoc");               // Optional
+        address.addValue("city", "Distrito Federal");
+        address.addValue("postal_code", "12345");
+        address.addValue("state", "Queretaro");
+        address.addValue("country_code", "MX");
+
+        this.customerRequest = new ParameterContainer("customer");
+        this.customerRequest.addValue("name", "John");
+        this.customerRequest.addValue("last_name", "Doe");
+        this.customerRequest.addValue("email", "johndoe@example.com");
+        this.customerRequest.addValue("phone_number", "554-170-3567");
+        this.customerRequest.addMultiValue(address);
+        
+        this.customer = this.api.customers().create(this.customerRequest.getParameterValues());
     }
 
     @After
@@ -50,13 +74,12 @@ public class CustomerTokenChargesTest extends BaseTest {
                 new SingleParameter("amount", "10.00"),
                 new SingleParameter("description", desc),
                 new SingleParameter("customer_language", "SP"),
-                new SingleParameter("capture", "FALSE"),
-                new SingleParameter("use_3d_secure", "FALSE"),
+                new SingleParameter("capture", "false"),
+                new SingleParameter("use_3d_secure", "false"),
                 new SingleParameter("use_card_points", "NONE"),
                 new SingleParameter("token", createToken()),
                 new SingleParameter("currency", "MXN"),
                 new SingleParameter("order_id", orderId)
-
         ));
 
         Charge transaction = this.api.charges().create(this.customer.getId(), tokenChargeParams);
@@ -69,7 +92,6 @@ public class CustomerTokenChargesTest extends BaseTest {
         assertNotNull(transaction);
         assertEquals(amount, transaction.getAmount());
         assertEquals(desc, transaction.getDescription());
-        Assert.assertNotNull(transaction.getFee());
         try {
             this.api.charges().get(transactionId);
             fail("Expected can't find");
